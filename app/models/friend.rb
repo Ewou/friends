1b265141e1ff838dc768917df44933208acefdbd
Ewou <ewmirowska@gmail.com>
2022-12-07 20:09:03 +0100

    Create app with scaffold

---
+++ app/models/friend.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: friends
+#
+#  id         :bigint           not null, primary key
+#  email      :string
+#  name       :string
+#  nickname   :string
+#  surname    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Friend < ApplicationRecord
+end
